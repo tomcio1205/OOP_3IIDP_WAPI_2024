a30fb80cb2f5904e05ce09a7022fbbf5404a6aee
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2024-12-19 07:23:11 +0100

    init 2

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db4fb4b3201421c4bde015da37ae8c157e5cde8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
